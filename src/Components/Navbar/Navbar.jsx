23d4df8e212aef44e25a005be78782ba8a7493cf
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-07 15:04:17 -0500

    navigation done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Navbar.css';
+import { Icon } from '@iconify/react';
 
 
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const openIcon = <Icon icon="gg:menu-right" className="open-icon"/>; 
+  const closeIcon = <Icon icon="eva:close-fill" className="close-icon"/>;
+  const logoIcon = <Icon icon="vscode-icons:file-type-htmlhint" />;
 
   return (
     <div>
-        <nav className="navbar">
-          <div className="logo-container">
-            <h2>Logo</h2>
-          </div>
+      <nav className="navbar" id="navbar-header">
 
-          <div className="navbar-link-container">
-              <a className="navbar-link" href="#">Home</a>
-              <a className="navbar-link" href="#">About Me</a>
-              <a className="navbar-link" href="#">My Stack</a>
-              <a className="navbar-link" href="#">Proyects</a>
-              <a className="navbar-link contact" href="#">Contact</a>
-          </div>
-          <button className="nav-mobile-icon">
-          </button>
-        </nav>
+        <header className="navbar-header">
+          <a href="#" className="brand-link">
+            {logoIcon}
+            <span className="brand">Logo</span>
+          </a>
+          <a href="#navbar-header" className="navbar-menu-mobile open">
+            {openIcon}
+          </a>
+          <a href="#" className="navbar-menu-mobile close">
+            {closeIcon}
+          </a>
+        </header>
+
+        <ul className="navbar-container">
+          <li className="navbar-item">
+            <a href="" className="navbar-link navbar-link-active">Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href="" className="navbar-link">About me</a>
+          </li>
+          <li className="navbar-item">
+            <a href="" className="navbar-link">My stack</a>
+          </li>
+          <li className="navbar-item">
+            <a href="" className="navbar-link">Proyects</a>
+          </li>
+          <li className="navbar-item">
+            <a href="" className="navbar-link contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
