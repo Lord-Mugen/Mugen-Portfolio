0db3b9cb8cc77889e6b69d813eab98d47c55aeee
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-08 12:10:24 -0500

    begining of the hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import './Hero.css';
+import perfil from '../../assets/images/perfil.jpeg';
 
 const Hero = () => {
+  const name = "Christian";
+
+
   return (
     <div>
-        
-        <h1>HERO</h1>
-
+      <section className="hero">
+        <div className="hero-image-container">
+          <img src={perfil} alt="portrait-image" className="portrait-image"/>
+        </div>
+        <div className="hero-h1-container">
+          <h1 className="hero-h1">I'm {name}</h1>
+          <h3 className="hero-h3">Full-Stack Web Developer</h3>
+        </div>
+      </section>
     </div>
   )
 }
