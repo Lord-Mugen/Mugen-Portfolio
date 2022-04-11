620735c9c17cf741b69a4782ebc76e1ce6c56b2a
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-11 16:45:57 -0500

    progress with slider section

--- src/Components/Contac/Contact.jsx
+++ src/Components/Contac/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Card from './Card';
 import './Contact.css';
 
 const Contact = () => {
--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -1,27 +1,39 @@
 import React from 'react'
 import './Proyects.css'
+import Replica from '../../assets/images/replica-web.svg';
+import Dom from '../../assets/images/toquen-el-dom.svg';
+import Covid from '../../assets/images/impacto-covid-19.svg';
+import Menta from '../../assets/images/menta-y-violeta.svg';
+import Crud from '../../assets/images/crud-php.svg';
+import Todo from '../../assets/images/mongo-todo-list.svg';
 
 const Proyects = () => {
   return (
     <div>
+      <div className="proyects-title-container">
          <h1 className="proyects-title">My <span>Proyects</span></h1>
+      </div>
         <section className="proyects">
             <div className="proyects-carrusel">
-                <div className="elements">1</div>
-                <div className="elements">2345678912345678</div>
-                <div className="elements">3456789234567890</div>
-                <div className="elements">4123456789023456</div>
-                <div className="elements">1234567895123456</div>
-                <div className="elements">1234567823456679</div>
-                <div className="elements">712345awefwrw678</div>
-                <div className="elements">81234567wgwrgwe9</div>
-                <div className="elements">9123452342343278</div>
-                <div className="elements">1234567234423910</div>
-                <div className="elements">1234567234423910</div>
-                <div className="elements">1234567234423910</div>
-                <div className="elements">1234567234423910</div>
-                <div className="elements">1234567234423910</div>
-                <div className="elements">1234567234423910</div>
+                <div className="elements">
+                  <a href="https://lord-mugen.github.io/ReplicaWeb/" target="_blank"><img src={Replica} alt="replicaWeb" className="proyects-image replic"/></a>
+                </div>
+                <div className="elements">
+                  <a href="https://elcabal.github.io/Toquen-el-DOM/" target="_blank"><img src={Dom} alt="toquen-el-dom" className="proyects-image"/></a>
+                </div>
+                <div className="elements">
+                  <a href="https://lord-mugen.github.io/Covid_19/" target="_blank"><img src={Covid} alt="impacto-covid" className="proyects-image"/></a>
+                </div>
+                <div className="elements">
+                  <a href="https://mentayvioleta.herokuapp.com/" target="_blank"><img src={Menta} alt="menta-y-violeta" className="proyects-image"/></a>
+                </div>
+                <div className="elements">
+                  <a href="http://consultorio-crud.infinityfreeapp.com/lo-tienes-crudo-consultorio/" target="_blank"><img src={Crud} alt="crud-php" className="proyects-image"/></a>
+                </div>
+                <div className="elements">
+                 <img src={Todo} alt="mongo-todo-list" className="proyects-image"/>
+                </div>
+                
             </div>
         </section>
     </div>
